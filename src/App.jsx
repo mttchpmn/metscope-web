9e2bd2d02ab1c684b3fa32dba6a14dee5d7e7359
Matt Chapman <matt@mttchpmn.com>
2019-07-01 22:06:18 +1200

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import WebcamPage from "./pages/WebcamPage";
+
+function App() {
+  return (
+    <div className="App">
+      <WebcamPage />
+    </div>
+  );
+}
+
+export default App;
