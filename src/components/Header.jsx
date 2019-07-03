a8a44fbccabfecba2f463683813db20449cf6778
Matt Chapman <matt@mttchpmn.com>
2019-07-03 14:38:14 +1200

    Implement baseline for sidebar nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import {
   Toolbar,
   IconButton,
   Typography,
-  makeStyles
+  makeStyles,
+  Drawer
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
@@ -19,17 +20,35 @@ const useStyles = makeStyles(theme => ({
 
 const Header = props => {
   const classes = useStyles();
+  const [state, setState] = React.useState({
+    drawerOpen: false
+  });
+
+  const openDrawer = () => {
+    setState({ ...state, drawerOpen: true });
+  };
+
+  const closeDrawer = () => {
+    setState({ ...state, drawerOpen: false });
+  };
+
   return (
-    <AppBar className={classes.appBar} position="static">
-      <Toolbar>
-        <IconButton edge="start">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Metscope
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <IconButton edge="start" onClick={() => openDrawer()}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Metscope
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer open={state.drawerOpen} onClose={() => closeDrawer()}>
+        NAV TO GO HERE
+      </Drawer>
+    </div>
   );
 };
 
