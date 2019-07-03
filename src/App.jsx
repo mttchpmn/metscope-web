2d9c6b7d5272b02f42d1dd1b057b6485a5448fb8
Matt Chapman <matt@mttchpmn.com>
2019-07-03 13:59:00 +1200

    Remove unneeded imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 
 import "./App.css";
 import WebcamPage from "./pages/WebcamPage";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
 import Header from "./components/Header";
 
 function App() {
