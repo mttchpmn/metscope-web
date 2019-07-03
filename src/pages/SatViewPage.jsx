5d4886088bd3f319cce343ff906395501f0bf10a
Matt Chapman <matt@mttchpmn.com>
2019-07-03 18:58:56 +1200

    Use https

--- src/pages/SatViewPage.jsx
+++ src/pages/SatViewPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import IFramePage from "./_IFramePage";
 
 const SatViewPage = props => {
-  return <IFramePage title="SatView" src="http://satview.bom.gov.au" />;
+  return <IFramePage title="SatView" src="https://satview.bom.gov.au" />;
 };
 
 export default SatViewPage;
--- src/pages/WindyPage.jsx
+++ src/pages/WindyPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import IFramePage from "./_IFramePage";
 
 const WindyPage = props => {
-  return <IFramePage title="Windy" src="http://embed.windy.com" />;
+  return <IFramePage title="Windy" src="https://embed.windy.com" />;
 };
 
 export default WindyPage;
