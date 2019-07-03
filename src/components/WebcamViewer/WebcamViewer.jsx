7e56503928bb3bb16cc6f311a19fa049c809ccfd
Matt Chapman <matt@mttchpmn.com>
2019-07-03 13:59:10 +1200

    Implement fullscreen modal

--- src/components/WebcamViewer/WebcamViewer.jsx
+++ src/components/WebcamViewer/WebcamViewer.jsx
@@ -7,9 +7,19 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import { CardHeader, IconButton, Container } from "@material-ui/core";
+import {
+  CardHeader,
+  IconButton,
+  Container,
+  Dialog,
+  AppBar,
+  Toolbar,
+  Paper,
+  Grid
+} from "@material-ui/core";
 import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import CloseIcon from "@material-ui/icons/Close";
 
 const moment = require("moment");
 
@@ -22,6 +32,20 @@ const styles = {
   media: {
     width: "100%",
     maxHeight: 400
+  },
+  appBar: {
+    position: "relative"
+    // marginBottom: 30
+  },
+  title: {
+    marginLeft: 30,
+    flex: 1
+  },
+  image: {
+    minWidth: 750,
+    maxWidth: "100%",
+    height: "auto",
+    maxHeight: 750
   }
 };
 
@@ -30,60 +54,135 @@ class WebcamViewer extends Component {
     super(props);
     this.state = {
       index: this.props.webcam.images.length - 1,
-      images: this.props.webcam.images
+      images: this.props.webcam.images,
+      fullScreen: false
     };
+  }
+
+  handleOpen() {
+    console.log("OPEN CALLED");
+    this.setState({ fullScreen: true });
+  }
+
+  handleClose() {
+    console.log("CLOSE CALLED");
+    this.setState({ fullScreen: false });
   }
 
   render() {
     const { classes, webcam } = this.props;
 
     return (
-      <Card className={classes.card}>
-        <CardHeader title={webcam.title} />
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            component="img"
-            height="500"
-            src={webcam.images[this.state.index].url}
-            title={webcam.title}
-          />
-          <Typography>
-            {moment(this.state.images[this.state.index].date).format(
-              "ddd h:mm a"
-            )}
-          </Typography>
-        </CardActionArea>
-        <CardActions>
-          <Container>
-            {this.state.index > 0 ? (
+      <div>
+        <Dialog
+          fullScreen
+          open={this.state.fullScreen}
+          onClose={() => this.handleClose()}
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar>
               <IconButton
-                onClick={() =>
-                  this.setState(prevState => ({
-                    index: prevState.index - 1
-                  }))
-                }
+                edge="start"
+                color="inherit"
+                onClick={() => this.handleClose()}
               >
-                <NavigateBeforeIcon />
+                <CloseIcon />
               </IconButton>
-            ) : null}
-            {this.state.index < webcam.images.length - 1 ? (
-              <IconButton
-                onClick={() =>
-                  this.setState(prevState => ({
-                    index: prevState.index + 1
-                  }))
-                }
-              >
-                <NavigateNextIcon />
-              </IconButton>
-            ) : null}
-          </Container>
-        </CardActions>
-        <CardContent>
-          <Typography>{webcam.desc}</Typography>
-        </CardContent>
-      </Card>
+              <Typography variant="h6" className={classes.title}>
+                {webcam.title}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Grid container spacing={0} direction="column" alignItems="center">
+            <Grid item xs={12}>
+              <img
+                className={classes.image}
+                src={webcam.images[this.state.index].url}
+              />
+              <Paper>
+                <Typography align="center" variant="h6">
+                  {moment(this.state.images[this.state.index].date).format(
+                    "ddd h:mm a"
+                  )}
+                </Typography>
+
+                <Container align="center">
+                  {this.state.index > 0 ? (
+                    <IconButton
+                      onClick={() =>
+                        this.setState(prevState => ({
+                          index: prevState.index - 1
+                        }))
+                      }
+                    >
+                      <NavigateBeforeIcon />
+                    </IconButton>
+                  ) : null}
+                  {this.state.index < webcam.images.length - 1 ? (
+                    <IconButton
+                      onClick={() =>
+                        this.setState(prevState => ({
+                          index: prevState.index + 1
+                        }))
+                      }
+                    >
+                      <NavigateNextIcon />
+                    </IconButton>
+                  ) : null}
+                </Container>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Dialog>
+
+        <Card className={classes.card}>
+          <CardHeader title={webcam.title} />
+          <CardActionArea onClick={() => this.handleOpen()}>
+            <CardMedia
+              className={classes.media}
+              component="img"
+              height="500"
+              src={webcam.images[this.state.index].url}
+              title={webcam.title}
+            />
+            <Typography>
+              {moment(this.state.images[this.state.index].date).format(
+                "ddd h:mm a"
+              )}
+            </Typography>
+          </CardActionArea>
+          <CardActions>
+            <Container>
+              {this.state.index > 0 ? (
+                <IconButton
+                  onClick={() =>
+                    this.setState(prevState => ({
+                      index: prevState.index - 1
+                    }))
+                  }
+                >
+                  <NavigateBeforeIcon />
+                </IconButton>
+              ) : null}
+              {this.state.index < webcam.images.length - 1 ? (
+                <IconButton
+                  onClick={() =>
+                    this.setState(prevState => ({
+                      index: prevState.index + 1
+                    }))
+                  }
+                >
+                  <NavigateNextIcon />
+                </IconButton>
+              ) : null}
+            </Container>
+          </CardActions>
+          <CardContent>
+            <Typography>{webcam.desc}</Typography>
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
