a4b9bab95dab03a51ebc9573d7ca959ed11ca955
Matt Chapman <matt@mttchpmn.com>
2019-07-03 12:59:44 +1200

    Update header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    marginBottom: 30
+  },
+  title: {
+    marginLeft: 30
+  }
+}));
+
 const Header = props => {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar className={classes.appBar} position="static">
       <Toolbar>
         <IconButton edge="start">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6">Metscope</Typography>
+        <Typography variant="h6" className={classes.title}>
+          Metscope
+        </Typography>
       </Toolbar>
     </AppBar>
   );
