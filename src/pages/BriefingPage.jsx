d47a45790f78200c744a8535a05ffc5cb5dee74f
Matt Chapman <matt@mttchpmn.com>
2019-12-21 17:42:08 +1300

    Complete bottom nav

--- src/pages/BriefingPage.jsx
+++ src/pages/BriefingPage.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { DataContext } from "../DataWrapper";
+import {
+  Container,
+  BottomNavigation,
+  BottomNavigationAction
+} from "@material-ui/core";
 
-const BriefingPage = () => {
-  return <div>Briefing Page</div>;
-};
+import AnnouncementIcon from "@material-ui/icons/Announcement";
+import AirplanemodeActiveIcon from "@material-ui/icons/AirplanemodeActive";
+import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import ToysIcon from "@material-ui/icons/Toys";
+import MapIcon from "@material-ui/icons/Map";
+import ImageIcon from "@material-ui/icons/Image";
+
+class BriefingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: "notam"
+    };
+  }
+
+  loadContainer(value) {
+    const lookup = {
+      notam: <div>NOTAM PAGE</div>,
+      aerodromes: <div>AERODROME PAGE</div>,
+      aaw: <div>AAW PAGE</div>,
+      charts: <div>CHARTS PAGE</div>,
+      webcams: <div>WEBCAM PAGE</div>,
+      metvuw: <div>METVUW PAGE</div>
+    };
+    return lookup[value];
+  }
+
+  render() {
+    return (
+      <Container>
+        <Container>{this.loadContainer(this.state.selectedTab)}</Container>
+
+        <BottomNavigation
+          value={this.state.selectedTab}
+          onChange={(event, newValue) => {
+            console.log("newValue :", newValue);
+            this.setState({ selectedTab: newValue });
+          }}
+        >
+          <BottomNavigationAction
+            value="notam"
+            label="NOTAM"
+            icon={<AnnouncementIcon />}
+          />
+          <BottomNavigationAction
+            value="aerodromes"
+            label="Aerodromes"
+            icon={<AirplanemodeActiveIcon />}
+          />
+          <BottomNavigationAction value="aaw" label="AAW" icon={<ToysIcon />} />
+          <BottomNavigationAction
+            value="charts"
+            label="CHARTS"
+            icon={<MapIcon />}
+          />
+          <BottomNavigationAction
+            value="webcams"
+            label="Webcams"
+            icon={<CameraAltIcon />}
+          />
+          <BottomNavigationAction
+            value="metvuw"
+            label="MetVUW"
+            icon={<ImageIcon />}
+          />
+        </BottomNavigation>
+      </Container>
+    );
+  }
+}
+BriefingPage.contextType = DataContext;
 
 export default BriefingPage;
