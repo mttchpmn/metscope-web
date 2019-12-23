5cfac1511d6b560dd51da28b2ac0de0cd60afc9b
Matt Chapman <matt@mttchpmn.com>
2019-12-23 20:09:58 +1300

    Base implementation of notam page

--- src/pages/containers/AerodromeContainer.jsx
+++ src/pages/containers/AerodromeContainer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { DataContext } from "../../DataWrapper";
+import SecurePage from "../../components/SecurePage";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent
+} from "@material-ui/core";
 
 class AerodromeContainer extends React.Component {
   constructor(props) {
@@ -7,8 +17,48 @@ class AerodromeContainer extends React.Component {
     this.state = {};
   }
 
+  aerodromeCard(aero) {
+    if (!aero.taf && !aero.metar && !aero.atis) return null;
+    return (
+      <Grid item key={aero.aerodrome} xs={12} sm={6} md={6} lg={4} xl={3}>
+        <Card>
+          <CardHeader title={aero.aerodrome} />
+          <CardContent>
+            <Typography>TAF</Typography>
+            <pre style={{ textAlign: "left" }}>
+              {aero.taf || "None received"}
+            </pre>
+
+            <Typography>METAR</Typography>
+            <pre style={{ textAlign: "left" }}>
+              {aero.metar || "None received"}
+            </pre>
+
+            <Typography>ATIS</Typography>
+            <pre style={{ textAlign: "left" }}>
+              {aero.atis || "None received"}
+            </pre>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
+
   render() {
-    return <div>Aerodrome Container</div>;
+    return (
+      <SecurePage>
+        {this.props.loading ? (
+          <LoadingSpinner />
+        ) : (
+          <Container align="center" maxWidth="xl">
+            <Typography variant="h6">Aerodromes</Typography>
+            <Grid container spacing={1}>
+              {this.props.data.map(aerodrome => this.aerodromeCard(aerodrome))}
+            </Grid>
+          </Container>
+        )}
+      </SecurePage>
+    );
   }
 }
 AerodromeContainer.contextType = DataContext;
--- src/pages/containers/NotamContainer.jsx
+++ src/pages/containers/NotamContainer.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent
+} from "@material-ui/core";
+
+import SecurePage from "../../components/SecurePage";
+import LoadingSpinner from "../../components/LoadingSpinner";
 import { DataContext } from "../../DataWrapper";
+import aerodromeLookup from "../../logic/aerodromeLookup";
 
 class NotamContainer extends React.Component {
   constructor(props) {
@@ -7,8 +19,51 @@ class NotamContainer extends React.Component {
     this.state = {};
   }
 
+  notamCard(aero) {
+    return (
+      <Grid item key={aero.aerodrome} xs={12} md={6}>
+        <Card>
+          <CardHeader
+            title={`${aerodromeLookup[aero.aerodrome]} (${aero.aerodrome})`}
+          />
+          <CardContent></CardContent>
+          {aero.notams.map(notam => {
+            return (
+              <Container>
+                <Grid container justify="space-between">
+                  <Grid item xs={6}>
+                    <Typography>{notam.series}</Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Typography>{notam.validity}</Typography>
+                  </Grid>
+                </Grid>
+                <pre style={{ textAlign: "left" }}>{notam.text}</pre>
+              </Container>
+            );
+          })}
+        </Card>
+      </Grid>
+    );
+  }
+
   render() {
-    return <div>NOTAM Container</div>;
+    return (
+      <SecurePage>
+        {this.props.loading ? (
+          <LoadingSpinner />
+        ) : (
+          <Container align="center" maxWidth="xl">
+            <Typography variant="h6">NOTAMs</Typography>
+            <Typography>Aerodromes</Typography>
+            <Grid container spacing={1}>
+              {this.props.data.map(aerodrome => this.notamCard(aerodrome))}
+            </Grid>
+          </Container>
+        )}
+      </SecurePage>
+    );
   }
 }
 NotamContainer.contextType = DataContext;
