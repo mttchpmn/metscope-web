60b2022c77f07233abcb11b71dc163d1f18c5307
Matt Chapman <matt@mattchapman.io>
2019-12-26 10:56:56 +1300

    Begin abstraction of WebcamViewer

---
+++ src/components/WebcamViewer/WebcamCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WebcamCard = props => {
+  return <WebcamCard></WebcamCard>;
+};
+
+export default WebcamCard;
---
+++ src/components/WebcamViewer/WebcamDialog.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+const WebcamDialog = props => {
+  return <Container></Container>;
+};
+
+export default WebcamDialog;
