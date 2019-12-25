d94a7ab4fe4343c9c1dfde035d11f88f0cb22057
Matt Chapman <matt@mattchapman.io>
2019-12-25 20:02:55 +1300

    Refactor to func component

--- src/pages/StartPage/index.jsx
+++ src/pages/StartPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Grid, Typography, Button } from "@material-ui/core";
 
@@ -6,64 +6,51 @@ import { DataContext } from "../../DataWrapper";
 import SecurePage from "../../components/SecurePage";
 import AreaMap from "./AreaMap";
 
-const areaMap = require("../../area-map.png");
-const areaMapMobile = require("../../area-map_mobile.png");
+const StartPage = () => {
+  return (
+    <SecurePage>
+      <Container maxWidth="xl" style={{ marginTop: 50 }}>
+        <DataContext.Consumer>
+          {data => (
+            <Grid
+              container
+              justify="center"
+              alignContent="center"
+              alignItems="center"
+              style={{ width: "100%", height: "100%" }}
+            >
+              <Grid item>
+                <Typography variant="h5" align="center">
+                  Select briefing areas
+                </Typography>
+                <Typography align="center">
+                  {data.getAreaList().length
+                    ? data.getAreaList().join(", ")
+                    : "Nothing selected"}
+                </Typography>
 
-class StartPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+                <AreaMap />
 
-  render() {
-    return (
-      <SecurePage>
-        <Container maxWidth="xl" style={{ marginTop: 20 }}>
-          <DataContext.Consumer>
-            {data => (
-              <Grid
-                container
-                justify="center"
-                alignContent="center"
-                alignItems="center"
-                style={{ width: "100%", height: "100%" }}
-              >
-                <Grid item>
-                  <Typography variant="h5" align="center">
-                    Select briefing areas
-                  </Typography>
-                  <Typography align="center">
-                    {data.getAreaList().length
-                      ? data.getAreaList().join(", ")
-                      : "Nothing selected"}
-                  </Typography>
-
-                  <AreaMap />
-
-                  <Container>
-                    <Grid item align="center">
-                      <Link to="/briefing">
-                        <Button
-                          variant="contained"
-                          color="inherit"
-                          onClick={() =>
-                            this.context.updateProp("areasSet", true)
-                          }
-                        >
-                          Get Briefing
-                        </Button>
-                      </Link>
-                    </Grid>
-                  </Container>
-                </Grid>
+                <Container>
+                  <Grid item align="center">
+                    <Link to="/briefing">
+                      <Button
+                        variant="contained"
+                        color="inherit"
+                        onClick={() => data.updateProp("areasSet", true)}
+                      >
+                        Get Briefing
+                      </Button>
+                    </Link>
+                  </Grid>
+                </Container>
               </Grid>
-            )}
-          </DataContext.Consumer>
-        </Container>
-      </SecurePage>
-    );
-  }
-}
-StartPage.contextType = DataContext;
+            </Grid>
+          )}
+        </DataContext.Consumer>
+      </Container>
+    </SecurePage>
+  );
+};
 
 export default StartPage;
