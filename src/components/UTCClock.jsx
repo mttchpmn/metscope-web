099649a4108b8b3d1a70b241e2cbb118f6efeb5e
Matt Chapman <matt@mattchapman.io>
2019-12-25 18:43:04 +1300

    Abstract clock into own component

---
+++ src/components/UTCClock.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+import moment from "moment";
+
+const UTCClock = props => {
+  const [time, setTime] = React.useState(
+    new moment().utc().format("DD MMM YYYY HH:mm:ss")
+  );
+
+  React.useEffect(() => {
+    setInterval(() => {
+      tick();
+    }, 1000);
+  });
+
+  const tick = () => {
+    setTime(
+      moment()
+        .utc()
+        .format("DD MMM YYYY HH:mm:ss")
+    );
+  };
+
+  return (
+    <Container>
+      <Typography align="center" style={{ color: "#505050" }}>
+        {time}
+      </Typography>
+    </Container>
+  );
+};
+
+export default UTCClock;
